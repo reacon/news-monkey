38aff84cce2d1f8d555d885887f0874e5e43fb13
Tanishq Pathak <61777674+reacon@users.noreply.github.com>
2024-02-08 21:36:18 +0530

    Update data.js

--- app/lib/data.js
+++ app/lib/data.js
@@ -3,7 +3,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 import { useQuery } from 'react-query';
 
 export async function fetchNews(){
-  const {data} = useQuery('news',() => fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=076789e4a20b445a8ea74bb75685dc33`).then((res)=>res.json()))
+  const {data} = useQuery('news',() => fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=`).then((res)=>res.json()))
     
 
   console.log(data)
