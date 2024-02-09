32673d4cd30ad99ca68856379d6b4c30c726df24
Looper <aayush.bhattacharjee2002@gmail.com>
2024-02-09 19:23:07 +0530

    displayed titles of articles

--- app/lib/data.js
+++ app/lib/data.js
@@ -2,6 +2,6 @@ import { unstable_noStore as noStore } from 'next/cache';
 import { useQuery } from 'react-query';
 
 export async function fetchNews() {
-  const res = await fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=`)
+  const res = await fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey`)
   return res.json()
 }
